faf823a1a55f3e7077de609ffeb64982baf8df36
ben-aslan <kiziltahaaslan@gmail.com>
2025-10-22 14:46:04 +0330

    fix(ban): if exist on banning ip

--- app/utils/ban.py
+++ app/utils/ban.py
@@ -1,18 +1,26 @@
 import subprocess
 
+
 def init_iptables():
     try:
-        subprocess.run(["ipset", "create", "banned", "hash:ip", "timeout", "0"], check=True, stderr=subprocess.DEVNULL)
+        subprocess.run(["ipset", "create", "banned", "hash:ip",
+                       "timeout", "0"], check=True, stderr=subprocess.DEVNULL)
     except subprocess.CalledProcessError:
         pass  # Already exists
 
     try:
-        subprocess.run(["iptables", "-C", "INPUT", "-m", "set", "--match-set", "banned", "src", "-j", "DROP"], check=True, stderr=subprocess.DEVNULL)
+        subprocess.run(["iptables", "-C", "INPUT", "-m", "set", "--match-set",
+                       "banned", "src", "-j", "DROP"], check=True, stderr=subprocess.DEVNULL)
     except subprocess.CalledProcessError:
-        subprocess.run(["iptables", "-A", "INPUT", "-m", "set", "--match-set", "banned", "src", "-j", "DROP"], check=True, stderr=subprocess.DEVNULL)
+        subprocess.run(["iptables", "-A", "INPUT", "-m", "set", "--match-set",
+                       "banned", "src", "-j", "DROP"], check=False, stderr=subprocess.DEVNULL)
+
 
 def ban_ip(ip: str, seconds: int):
-    subprocess.run(["ipset", "add", "banned", ip, "timeout", str(seconds)], check=True, stderr=subprocess.DEVNULL)
-    
+    subprocess.run(["ipset", "add", "banned", ip, "timeout", str(
+        seconds), "-exist"], check=True, stderr=subprocess.DEVNULL)
+
+
 def unban_ip(ip: str, seconds: int):
-    subprocess.run(["ipset", "del", "banned", ip], check=True, stderr=subprocess.DEVNULL)
+    subprocess.run(["ipset", "del", "banned", ip],
+                   check=False, stderr=subprocess.DEVNULL)
